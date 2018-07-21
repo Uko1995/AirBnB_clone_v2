6e0fadb55d364aa33954bb919e9a7409bb36f7bb
Dkazem91 <Dkazemian@gmail.com>
2018-07-21 16:18:19 +0000

    typo fix

--- models/base_model.py
+++ models/base_model.py
@@ -66,7 +66,7 @@ class BaseModel:
         cp_dct['__class__'] = self.__class__.__name__
         cp_dct['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         cp_dct['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        cp_dict.pop('_sa_instance_state', None)
+        cp_dct.pop('_sa_instance_state', None)
 
         return (cp_dct)
 
