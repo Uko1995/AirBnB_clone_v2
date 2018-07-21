0011bb2c1d8c84f7eae0399c847448f022b81317
Dkazem91 <Dkazemian@gmail.com>
2018-07-21 12:50:07 +0000

    added state and city updated

--- models/city.py
+++ models/city.py
@@ -2,12 +2,15 @@
 '''
     Define the class City.
 '''
-from models.base_model import BaseModel
+from models.base_model import Basemodel, Base
+from models.state import State
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-
-class City(BaseModel):
+class City(BaseModel, Base):
     '''
         Define the class City that inherits from BaseModel.
     '''
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    name = Column(String(128),nullable=False)
--- models/state.py
+++ models/state.py
@@ -2,12 +2,18 @@
 '''
     Implementation of the State class
 '''
+from sqlalchemy import Column, Integer, String
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
+class State(BaseModel, Base):
     '''
         Implementation for the State.
     '''
-    name = ""
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
+    @property
+    def cities(self):
+        return [city for city in State.cities if city.state_id == self.id]
