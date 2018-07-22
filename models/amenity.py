4e4d7c2ddf30c9d8b88c4148736c94becbe863d2
Dkazem91 <Dkazemian@gmail.com>
2018-07-22 12:14:57 +0000

    updated amenities for sqlalchemy and relationship

--- models/amenity.py
+++ models/amenity.py
@@ -2,11 +2,16 @@
 '''
     Implementation of the Amenity class
 '''
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel):
     '''
         Implementation for the Amenities.
     '''
-    name = ""
+    __tablename__ = "amenities"
+    name = Column(String(128), nullable=False)
+    place_amenities = relationship("Place",
+                                   secondary="place_amenity",
+                                   back_populates="amenities")
