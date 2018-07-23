109b1a76ad3d2f81f7a9813bdeb53ce56a0c33ba
Mikaelia <mikaelagurney@gmail.com>
2018-07-23 00:54:54 +0000

    Add task 9 and 10 and debug

--- models/amenity.py
+++ models/amenity.py
@@ -5,13 +5,14 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.place import place_amenity
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     '''
         Implementation for the Amenities.
     '''
     __tablename__ = "amenities"
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place",
-                                   secondary="place_amenity",
+                                   secondary=place_amenity,
                                    back_populates="amenities")
