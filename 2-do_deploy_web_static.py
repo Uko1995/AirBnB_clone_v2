702e785d551bfff2ab1cc8e81d498210e0764d28
Dkazem91 <Dkazemian@gmail.com>
2018-07-30 14:36:23 +0000

    fixed with backslashes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -33,10 +33,12 @@ def do_deploy(archive_path):
         run('mkdir -p /data/web_static/releases/{}'.format(fd))
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(fd, fd))
         run('rm /tmp/{}'.format(fd))
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(fd, fd))
+        run('mv /data/web_static/releases/{}/web_static/*\
+        /data/web_static/releases/{}/'.format(fd, fd))
         run('rm -rf /data/web_static/releases/{}/web_static'.format(fd))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(fd))
+        run('ln -s /data/web_static/releases/{}/\
+        /data/web_static/current'.format(fd))
         print("New version deployed!")
         return True
     except:
