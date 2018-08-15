81a03bb1207bb46706ba627c4f89c3b5d8dec8c1
Dkazem91 <Dkazemian@gmail.com>
2018-08-15 00:29:35 +0000

    added 0 pycode styled

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -5,10 +5,11 @@ from flask import Flask
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
+
 @app.route('/')
 def hello_world():
     # prints hello
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 if __name__ == "__main__":
